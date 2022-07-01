ad3a5c6e6578b8d5e71e5e6a395459a69af65162
LalaChristl <lesleyannchristl@gmail.com>
2022-07-01 10:59:17 +0200

    added js file

---
+++ solution.js
@@ -0,0 +1,28 @@
+let snackServing = 30;
+let guests = 15;
+console.log(snackServing, guests);
+console.log(snackServing - guests);
+console.log(guests - snackServing);
+console.log(snackServing * guests);
+console.log(snackServing / guests);
+let drinks = 10;
+console.log((snackServing * guests) / drinks);
+resultOne = 45;
+console.log(resultOne);
+let monkeys = 15;
+let bananas = 9;
+console.log(monkeys / bananas);
+let monkeyFights = 20;
+console.log((monkeys + bananas) * monkeyFights);
+let resultTwo = 480;
+let monkeyBusiness = 90;
+monkeyBusiness++;
+let boredom = 13;
+boredom--;
+console.log(monkeys - bananas);
+let happiness = 6;
+console.log(happiness + monkeyFights);
+console.log(resultOne / resultTwo);
+
+
+
